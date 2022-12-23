aa660e98a7661dd6db8205c3d2ae030777693264
matcom-chacha <gabubabuchi@gmail.com>
2022-12-23 01:20:36 -0500

    Add CaClient class with basic methods

---
+++ FabricCaClient/FabricCaClient/CAClient.cs
@@ -0,0 +1,30 @@
+﻿namespace FabricCaClient
+{
+    /// <summary>
+    ///  A class that encapsulates a set of methods to communicate with Hyperledger Fabric (HF)'s Certificate Authority (CA).
+    /// </summary>
+    public class CAClient
+    {
+		/// <summary>
+		/// Enroll an identity
+		/// </summary>
+		/// /// <param name="x"></param>
+		/// <returns></returns>
+		public void enroll() { }
+
+		/// <summary>
+		/// Reenroll an identity
+		/// </summary>
+		public void reenroll() { }
+
+		/// <summary>
+		/// Register an identity
+		/// </summary>
+		public void register() { }
+
+		/// <summary>
+		/// Revoke an identity
+		/// </summary>
+		public void revoke() { }
+	}
+}
